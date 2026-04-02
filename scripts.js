f3473541177cd3641792f0448995ed7b903e1c94
Miranda Bouck <boucksy@users.noreply.github.com>
2026-04-02 09:21:43 -0500

    add hawkins slug and a11y fixes

--- scripts.js
+++ scripts.js
@@ -4,4 +4,9 @@ document.querySelectorAll('a[target="_blank"]').forEach(function (link) {
   icon.className = 'external-link-icon';
   icon.textContent = ' ↗';
   link.appendChild(icon);
+
+  var srText = document.createElement('span');
+  srText.className = 'sr-only';
+  srText.textContent = '(opens in new tab)';
+  link.appendChild(srText);
 });
